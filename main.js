876fdc3687898b832043f51a53e371d7c83650b1
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-08-15 05:14:44 +0300

    deploy

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello Bulma!');
 });
+
+var create = document.getElementById('create-btn');
+
+create.addEventListener('click', function (e) {
+  e.preventDefault();
+  var modal = document.getElementById('card-modal');
+  modal.classList.toggle('is-active');
+});
+
+//close modal and refresh forms
