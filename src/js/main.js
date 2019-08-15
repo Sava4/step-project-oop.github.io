8f46d59b2512c9ada529b176ccc063e2227e279c
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-08-15 05:13:28 +0300

    Basic layout section with modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+const create = document.getElementById('create-btn');
+
+create.addEventListener('click', (e) => {
+  e.preventDefault();
+  let modal = document.getElementById('card-modal');
+  modal.classList.toggle('is-active');
+})
+
+//close modal and refresh forms
